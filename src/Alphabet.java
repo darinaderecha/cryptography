c9bbdc29b9bb3a0ac3db4654da5d8651ddc495cc
darina_derecha <darinaderechaa@gmail.com>
2022-05-18 12:24:21 +0300

    alphabet is made

---
+++ src/Alphabet.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Alphabet {
+
+    public List<Character> alphabet;
+
+
+    public  List<Character> ordinaryAlphabet(){
+        alphabet = new ArrayList<>();
+        char startAlphabet = 'А';
+        char endAlphbet = 'я';
+        for (int i = startAlphabet; i <= endAlphbet; i++) {
+            alphabet.add((char) i);
+
+        }
+        alphabet.add('Ё');
+        alphabet.add('ё');
+        alphabet.add('.');
+        alphabet.add(',');
+        alphabet.add('"');
+        alphabet.add(':');
+        alphabet.add('-');
+        alphabet.add('!');
+        alphabet.add('?');
+        alphabet.add(' ');
+
+
+        return alphabet;
+
+
+    }
+}
