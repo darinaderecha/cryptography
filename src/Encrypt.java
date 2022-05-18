2b2f9de027c66aec84e0b90203797d3c8b47adcd
darina_derecha <darinaderechaa@gmail.com>
2022-05-18 23:01:31 +0300

    a class for working with files is created

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -1,24 +1,26 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class Encrypt {
-     private final int cipherKey = 10;
+     private final static int cipherKey = 10;
+     Alphabet alphabet = new Alphabet();
 
 
-    public String encrypt(String message, List<Character> alphabet){
+    public String encrypt(String message){
+        List<Character> al = alphabet.ordinaryAlphabet();
+
 
         char[] charsFromMessage = message.toCharArray();
 
         StringBuilder builder = new StringBuilder(message.length());
 
         for (int i = 0; i < charsFromMessage.length; i++) {
-            for (int j = 0; j < alphabet.size(); j++) {
+            for (int j = 0; j < al.size(); j++) {
 
-                if(alphabet.get(j) == charsFromMessage[i]){
-                    if(j >= alphabet.size() - cipherKey){
-                        charsFromMessage[i] = alphabet.get(j - alphabet.size() - 1 + cipherKey);
+                if(al.get(j) == charsFromMessage[i]){
+                    if(j >= al.size() - cipherKey){
+                        charsFromMessage[i] = al.get(j - al.size() - 1 + cipherKey);
                     } else {
-                        charsFromMessage[i] = alphabet.get(j + cipherKey);
+                        charsFromMessage[i] = al.get(j + cipherKey);
                     }
                     builder.append(charsFromMessage[i]);
                     break;
